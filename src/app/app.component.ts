5846226f455b9de7f953f13e239ca217f646d95c
Angel Romero <angel.romero@globant.com>
2017-04-17 12:47:18 -0500

    implementing slick library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterContentInit } from '@angular/core';
+import * as $ from 'jquery';
+import 'slick-carousel';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements AfterContentInit {
   title = 'app works!';
+  $element: any;
+  constructor(private el: ElementRef) {
+  }
+
+  ngAfterContentInit() {
+    $('#mainSlide').slick();
+  }
 }
